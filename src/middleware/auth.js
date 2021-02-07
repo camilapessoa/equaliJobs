fe10fc67353ea08d0c70de9020d3b66584d1121a
camilapessoa <camilabentespessoa@gmail.com>
2021-02-07 20:15:04 -0300

    add auth

---
+++ src/middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+const SECRET_USER = process.env.SECRET_USER
+
+
+const auth = (req, res, next) => {
+
+  const authHeader = req.get('authorization')
+
+  if (!authHeader)
+    return res.status(401).send({ message: 'Token não localizado.' })
+
+  const token = authHeader.split(' ')[1]
+
+  jwt.verify(token, SECRET_USER, (error, decoded) => {
+    if (error)
+      return res.status(401).send({ message: 'Token inválido.' })
+
+    req.userId = decoded.id
+    return next()
+  })
+
+}
+
+module.exports = { auth }
