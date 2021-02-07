f20769da753ca2fcb3bf5665b2f13650eee0539a
camilapessoa <camilabentespessoa@gmail.com>
2021-02-07 19:48:46 -0300

    add database connection

---
+++ src/model/companySchema.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
+
+const companySchema = new Schema({
+
+  name: {
+    type: String,
+    required: true
+  },
+  cnpj: {
+    type: String,
+    required: true
+  },
+  endereco: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+
+  contato: {
+    type: String,
+    required: true
+  },
+
+  password: {
+    type: String,
+    required: true,
+    select: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  versionKey: false
+});
+
+companySchema.pre('save', async function (next) {
+  const hash = await bcrypt.hash(this.password, 10)
+  this.password = hash
+
+  next()
+})
+
+const companyCollections = mongoose.model('company', companySchema);
+
+module.exports = companyCollections
