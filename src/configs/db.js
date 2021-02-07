a27efc5692ed8b7bf7ab96586140cf96c595c21f
camilapessoa <camilabentespessoa@gmail.com>
2021-02-07 19:12:48 -0300

    add database connection

---
+++ src/configs/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const mongoUri = process.env.MONGO_URI //mongodb://localhost:27017/
+
+const connect = () => {
+  mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: falsegit
+  })
+
+    .then(() => console.log('Conectamos no Mongo.'))
+    .catch((err) => console.log(err))
+
+}
+
+module.exports = { connect }
