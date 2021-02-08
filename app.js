cae60ad8cea22f928eb8c4e2a6fb3991c3b0f93d
camilapessoa <camilabentespessoa@gmail.com>
2021-02-07 21:30:24 -0300

    refactor: app

--- app.js
+++ app.js
@@ -11,13 +11,15 @@ app.use(express.json())
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const index = require('../routes/index')
+const index = require('../equaliJobs/src/routes/index')
 const company = require('../equaliJobs/src/routes/companyRoute')
 const userAccount = require('../equaliJobs/src/routes/accountRoute')
 
 app.use('/', index)
 app.use('/account', userAccount)
 app.use('/company', company)
+
+//mongodb+srv://admin_hogar:7bcZSOJMXJJxpWMg@apihogarcluster.xhulk.mongodb.net/equalijobs?retryWrites=true&w=majority
 
 
 
