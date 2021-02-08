2e3b6837101cece4ed9ff33f66566f8199b36c0c
camilapessoa <camilabentespessoa@gmail.com>
2021-02-08 16:18:32 -0300

    fix: main route entrypoint

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/api/v1', userRouter)
 app.use('/api/v1', companyRouter)
-app.use('/api/v1', mainRouter)
+app.use('/', mainRouter)
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Não foi possível encontrar ${req.originalUrl} neste servidor.`, 404));
