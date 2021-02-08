261dc2861113e7c3df79c256e6f73ecf2b7d8065
camilapessoa <camilabentespessoa@gmail.com>
2021-02-08 16:14:29 -0300

    feat: add main route

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const userRouter = require('./routes/userRoutes')
 const companyRouter = require('./routes/companyRoutes')
+const mainRouter = require('./routes/mainRoute')
 
 
 app.use(cors())
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/api/v1', userRouter)
 app.use('/api/v1', companyRouter)
+app.use('/api/v1', mainRouter)
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Não foi possível encontrar ${req.originalUrl} neste servidor.`, 404));
